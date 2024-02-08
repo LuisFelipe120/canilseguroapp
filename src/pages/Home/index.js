033dac40cf3bd489b7b30ec9d37cde4af90e2be6
BRENO DE OLIVEIRA RODRIGUES <breno.orodrigues1@senacsp.edu.br>
2024-02-07 22:15:22 -0300

    adicionei alguns detalhes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,34 +7,39 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <section className='linha'>
-        <div className='box'>
-          <h1 className='title'>Canil CollieEsporte</h1>
-          <br/>
-          <p className='city'>Presidente Prudente, SP</p>
-          <br/>
-          <p className='contact'>CollieEsporte@gmail.com</p>
-          <br/>
-          <a href='www.collieOficial.com.br' className='link'>www.collieOficial.com.br</a>
-          <br/>
-          <p className='text'>O Canil Collie Esporte, é um canil de <br/>
-          raça pura Border Collie,afiliado com a <br/>
-          cbkc com dez anos de qualidade</p>
-          <br/>
-          <img src={Dog}/>
-          <br/>
-          <img src={dog}/>
-          <br/>
-          <div className='inb'>
-            <p className='favory'>Favoritos</p>
-          </div>
-          <br/>
-          <p className='avali'>Avaliações Geral</p>
-          <br/>
-          <div id='bord'>
-            <div className='bord'>
-              <p id='avali'>Avaliar Geral</p>
+      <section>
+        <div className='linha'>
+          <div className='box'>
+            <h1 className='title'>Canil CollieEsporte</h1>
+            <br/>
+            <p className='city'>Presidente Prudente, SP</p>
+            <br/>
+            <p className='contact'>CollieEsporte@gmail.com</p>
+            <br/>
+            <a href='www.collieOficial.com.br' className='link'>www.collieOficial.com.br</a>
+            <br/>
+            <p className='text'>O Canil Collie Esporte, é um canil de <br/>
+            raça pura Border Collie,afiliado com a <br/>
+            cbkc com dez anos de qualidade</p>
+            <br/>
+            <img src={Dog}/>
+            <br/>
+            <img src={dog}/>
+            <br/>
+            <div className='inb'>
+              <p className='favory'>Favoritos</p>
             </div>
+            <br/>
+            <p className='avali'>Avaliações Geral</p>
+            <br/>
+            <div id='bord'>
+              <div className='bord'>
+                <p id='avali'>Avaliar este Canil</p>
+              </div>
+              <br/>
+              <input type="text" className='userInput'/>
+            </div>
+            <br/>
           </div>
         </div>
       </section>
